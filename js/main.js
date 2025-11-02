ef4ab9c62526d9ae4dbec24269e9fd8ea5db852c
Soumya Chatterjee <soumyachatterjee@Soumyas-MacBook-Air.local>
2025-11-02 16:00:11 +0530

    jwt add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_BASE_URL = 'http://localhost:3000'; 
+const API_BASE_URL = 'https://soumya-doctor-3.onrender.com'; 
 
 // Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
